7ba7526000df6b929e4d51ea3ec631c4ca30e1aa
Carlo Calderon <carlo.calderon@timining.cl>
2024-01-26 13:11:15 -0300

    Se puede rotar en eje Y y apuntar hacia la pelota

--- car.gd
+++ car.gd
@@ -5,47 +5,54 @@ const STEER_LIMIT = 0.7
 const MIN_POSITION_Y = 0.1
 
 @export var engine_force_value = 400
-@export var Jump_impulse = 50.0
+@export var Jump_impulse = 3.0
+@export var rotation_speed = 3.0
 
 var steer_target = 0
+var esta_volando = false
 
 signal movido
 
 func _physics_process(delta):
 #	var fwd_mps = transform.basis.xform_inv(linear_velocity).x
 	var fwd_mps = (transform.basis.inverse()*linear_velocity).x
+	esta_volando = not(position.y < MIN_POSITION_Y)
 
 	steer_target = Input.get_action_strength("rotate_left") - Input.get_action_strength("rotate_right")
 	steer_target *= STEER_LIMIT
 
-	if Input.is_action_pressed("move_forward"):
-		# Increase engine force at low speeds to make the initial acceleration faster.
-		var speed = linear_velocity.length()
-		if speed < 5 and speed != 0:
-			engine_force = clamp(engine_force_value * 5 / speed, 0, 100)
-		else:
-			engine_force = engine_force_value
+	if esta_volando==true:
+		print("volando!!", steer_target)
+		angular_velocity = Vector3(0, steer_target*rotation_speed, 0)
 	else:
-		engine_force = 0
-
-	if Input.is_action_pressed("move_back"):
-		# Increase engine force at low speeds to make the initial acceleration faster.
-		if fwd_mps >= -1:
+		if Input.is_action_pressed("move_forward"):
+			# Increase engine force at low speeds to make the initial acceleration faster.
 			var speed = linear_velocity.length()
 			if speed < 5 and speed != 0:
-				engine_force = -clamp(engine_force_value * 5 / speed, 0, 100)
+				engine_force = clamp(engine_force_value * 5 / speed, 0, 100)
 			else:
-				engine_force = -engine_force_value
+				engine_force = engine_force_value
 		else:
-			brake = 1
-	else:
-		brake = 0.0
+			engine_force = 0
 
-	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
+		if Input.is_action_pressed("move_back"):
+			# Increase engine force at low speeds to make the initial acceleration faster.
+			if fwd_mps >= -1:
+				var speed = linear_velocity.length()
+				if speed < 5 and speed != 0:
+					engine_force = -clamp(engine_force_value * 5 / speed, 0, 100)
+				else:
+					engine_force = -engine_force_value
+			else:
+				brake = 1
+		else:
+			brake = 0.0
+
+		steering = move_toward(steering, steer_target, STEER_SPEED * delta)
 	
-	if Input.is_action_pressed("jump") and position.y < MIN_POSITION_Y:
-		#apply_impulse(Vector3(0, Jump_impulse, 0), Vector3(0, 0, 0.0))
-		linear_velocity = linear_velocity + Vector3(0, 3, 0)
+		if Input.is_action_pressed("jump"):
+			#apply_impulse(Vector3(0, Jump_impulse, 0), Vector3(0, 0, 0.0))
+			linear_velocity = linear_velocity + Vector3(0, Jump_impulse, 0)
 		
 	if Input.is_action_pressed("reset_car"):
 		rotation = Vector3(0, 0, 0)
@@ -54,5 +61,6 @@ func _physics_process(delta):
 		movido.emit()
 
 func _on_timer_timeout():
-	print(linear_velocity.length(), " ; ", linear_velocity, " ; ", get_contact_count())
-	print("Engine: ", engine_force, ", Brake: ", brake, ", Steering: ", steering)
+	print("Volando? ", esta_volando)
+	#print(linear_velocity.length(), " ; ", linear_velocity, " ; ", get_contact_count())
+	#print("Engine: ", engine_force, ", Brake: ", brake, ", Steering: ", steering)
--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-enum CameraMode {ThrethPerson, FirstPerson, Fixed, Modes}
+enum CameraMode {ThrethPerson, FirstPerson, Fixed, Ball, Modes}
 
 @export var camera_distance = Vector3(0, 5, 20)
 @export var camera_mode:int = CameraMode.ThrethPerson
@@ -38,15 +38,24 @@ func _on_tanque_movido():
 
 func _on_car_movido():
 	if camera_mode == CameraMode.ThrethPerson:
-		$CameraPivot/Camera3D.position = Vector3(0, 3, 10)
+		$CameraPivot/Camera3D.position = Vector3(0, 2, 10)
 		$CameraPivot.position = $Car.position
+		$CameraPivot/Camera3D.look_at($Car.position, Vector3.UP)
 		$CameraPivot.rotation.x = -PI/6.0
 		$CameraPivot.rotation.y = $Car.rotation.y + PI
 	if camera_mode == CameraMode.FirstPerson:
-		$CameraPivot/Camera3D.position = Vector3(0, 3, -3)
+		$CameraPivot/Camera3D.position = Vector3(0, 1.5, -1)
 		$CameraPivot.position = $Car.position
 		$CameraPivot.rotation.x = 0
-		$CameraPivot.rotation.y = $Player/Pivot.rotation.y-PI/2.0
+		$CameraPivot.rotation.y = $Car.rotation.y - PI
 	if camera_mode == CameraMode.Fixed:
 		$CameraPivot/Camera3D.position = Vector3(0, 20, 20)
 		$CameraPivot.rotation.x = -PI/4.0
+	if camera_mode == CameraMode.Ball:
+		var objective_position = $Ball.global_transform.origin
+#		print($Ball.position, " ; ", $Ball.position+$Ball/Pivot.position)
+		var direction = ($Ball.position - $CameraPivot.position).normalized()
+		$CameraPivot/Camera3D.position = Vector3(0, 3, 10)
+		$CameraPivot.position = $Car.position - 10*direction
+		$CameraPivot/Camera3D.look_at(objective_position+0*direction, Vector3.UP)
+		
